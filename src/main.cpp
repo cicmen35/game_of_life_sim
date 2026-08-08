cc15f85b21b65ccdbbd0d186c15e924ce2f74d3e
cicmen35 <138312285+cicmen35@users.noreply.github.com>
2026-08-08 20:14:12 +0200

    feat: implement grid class and add test cells to simulation loop

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,19 @@
+#include "grid.h"
 #include <raylib.h>
 
 int main() {
   Color Grey = {29, 29, 29, 255};
   const int WINDOW_WIDHT = 750;
   const int WINDOW_HEIGHT = 750;
+  const int CELL_SIZE = 25;
   int FPS = 12;
 
   InitWindow(WINDOW_WIDHT, WINDOW_HEIGHT, "Game of Life");
   SetTargetFPS(FPS);
+  Grid grid(WINDOW_WIDHT, WINDOW_HEIGHT, CELL_SIZE);
+  grid.SetValue(0, 0, 1);
+  grid.SetValue(2, 1, 1);
+  grid.SetValue(0, 0, 80);
 
   // Simulation loop
   while (WindowShouldClose() == false) {
@@ -18,6 +24,7 @@ int main() {
     // 3. Draw
     BeginDrawing();
     ClearBackground(Grey);
+    grid.Draw();
     EndDrawing();
   }
 
